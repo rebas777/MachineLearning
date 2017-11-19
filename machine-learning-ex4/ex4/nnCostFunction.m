ef2957316247851c3984601dc2d70aeb0c0f7f8a
rebas777 <154587260@qq.com>
2017-11-19 16:54:46 +0800

    finish ex4

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -24,6 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
+K = num_labels;
+L = 3; 
          
 % You need to return the following variables correctly 
 J = 0;
@@ -62,25 +64,104 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+function output = cost(h0xi, yi)
+	output = 0;
+	K = size(h0xi, 1);
+	for k = 1:K
+        output += yi(k)*log(h0xi(k)) + (1-yi(k))*log(1-h0xi(k));
+	end
+end
+
+function output = cookY(yi_raw, K)
+    output = zeros(K,1);
+    output(yi_raw) = 1;
+end
+
+for i = 1:m
+	yi_raw = y(i);
+    Xi = X(i,:);
+    a1 = [1; Xi'];  
+    z2 = Theta1*a1;
+    a2 = [1; sigmoid(z2)];
+    z3 = Theta2*a2;
+    h0xi = sigmoid(z3);
+    yi = cookY(yi_raw, K);  %transform yi_raw from a sigle number to a 0/1 vector 
+    J += cost(h0xi, yi);
+end
+
+J = (-1)*J/m;
+
+%now add regularization part
+
+reg = 0;
+
+for j = 1:hidden_layer_size
+	for k = 2:input_layer_size+1
+        reg += Theta1(j,k)^2;
+	end
+end 
+
+for j = 1:num_labels
+	for k = 2:hidden_layer_size+1
+		reg += Theta2(j,k)^2;
+	end
+end
+
+reg = reg * lambda /(2*m);
+
+J = J + reg;
 
 
 
 
+% grads------------------------------------------------------------
+
+DELTA1 = zeros(size(Theta1));
+DELTA2 = zeros(size(Theta2));
+for t = 1:m
+    %step 1
+    Xi = X(t,:);
+    a1 = [1; Xi'];  
+    z2 = Theta1*a1;
+    a2 = [1; sigmoid(z2)];
+    z3 = Theta2*a2;
+    a3 = sigmoid(z3);
+    yt_raw = y(t);
+    yt = cookY(yt_raw, K);  %transform yi_raw from a sigle number to a 0/1 vector
+
+    %step 2
+    delta3 = zeros(num_labels,1);
+    delta3 = a3 - yt;
+
+    %step 3
+    %delta2 = (Theta2'*delta3).*[1;sigmoidGradient(z2)];  
+    delta2 = (Theta2'*delta3).*sigmoidGradient([1;z2]);
+    %problem: different demension. Should i put bias unit in or out of the sigmoidGradient function?
 
 
+    %step 4
+    DELTA1 += delta2(2:end)*a1';
+    DELTA2 += delta3*a2';
 
+end
 
+%step 5
+Theta1_grad = DELTA1/m;
+Theta2_grad = DELTA2/m;
 
+%Regularization
+for i = 1:hidden_layer_size
+    for j = 2:(input_layer_size + 1)
+        Theta1_grad(i,j) = Theta1_grad(i,j) + lambda*(Theta1(i,j))/m;
+    end
+end
 
+for i = 1:num_labels
+	for j = 2:(hidden_layer_size + 1)
+		Theta2_grad(i,j) = Theta2_grad(i,j) + lambda*(Theta2(i,j))/m;
+	end
+end
 
-
-
-
-
-
-
-
-% -------------------------------------------------------------
 
 % =========================================================================
 
--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -11,7 +11,8 @@ function W = randInitializeWeights(L_in, L_out)
 
 % You need to return the following variables correctly 
 W = zeros(L_out, 1 + L_in);
-
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
 %               training the neural network.
